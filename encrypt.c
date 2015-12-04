724f7df05bfa291364abd11b8843272eb2eb66b9
Nicholas Koh <nicholaskohwx@gmail.com>
2015-12-04 12:09:32 +0800

    improved longer keys
    Signed-off-by: Nicholas Koh <nicholaskohwx@gmail.com>

--- encrypt.c
+++ encrypt.c
@@ -1,6 +1,13 @@
-#include "util.h"
-#include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void seedRandom();
+long primeNumberAbove(long in);
+long randomInt(long min, long max);
+int isPrime(long x);
 
 typedef struct{
 	int n;
@@ -8,22 +15,56 @@ typedef struct{
 	int d;
 } Keys;
 
+void seedRandom(){
+	time_t t;
+	srand((unsigned)time(&t));
+}
+long primeNumberAbove(long in){
+	in++;
+	for(;;in++){
+		if (in % 2 == 0){
+			continue;
+		}
+		if(isPrime(in)==0){
+			return in;
+		}
+	}
+}
+
+long randomInt(long min, long max){
+	return rand()%(max-min) + min;
+}
+
+int isPrime(long x){
+	if (x % 2 == 0){
+		return 1;
+	}
+	long max = floor(sqrt(x));
+	int i = 2;
+	for (; i < max; i++) {
+		if (x % ((2 * i) + 1) == 0){
+			return 1;
+		}
+	}
+	return 0;
+}
 void encrypt(){
 	seedRandom();
-	int p = primeNumberAbove(randomInt(0,100));
-	int q = primeNumberAbove(p);
+	long p = primeNumberAbove(randomInt(1000,2000));
+	long q = primeNumberAbove(p);
 	
-	int n = p*q;
-	int tot = (p-1)*(q-1);
+	long n = p*q;
+	long tot = (p-1)*(q-1);
 	
 	int i;
-	int d[10];
+	long d[10];
 	printf("Possible values of d & e\n");
 	
 	do{
 		i++;
 		d[i-1] = tot*i+1;
-		int e = d[i-1],div=2;
+		long e = d[i-1];
+		int div=2;
 		
 		do{
 			if(e%div==0){
@@ -38,7 +79,7 @@ void encrypt(){
 		}
 	}while(i<=10);
 	
-
+	
 	
 	printf("\np: %d\nq: %d\nn: %d\ntot: %d\n",p,q,n,tot);
 }
