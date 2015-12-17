cd237a9f2fef690dbbd408667f78aa20c0051c0c
Nicholas Koh <nicholaskohwx@gmail.com>
2015-12-17 17:37:32 +0800

    seperating them for easier testing
    Signed-off-by: Nicholas Koh <nicholaskohwx@gmail.com>

--- file.c
+++ file.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void encryptFile(char* name){
 	
@@ -11,15 +12,25 @@ void encryptFile(char* name){
 	in = (char*)malloc(size);
 	size_t ret_code = fread(in, sizeof *in, 1, fp); 
 	printf("%d\n",size);
-	printf("%d",ret_code);
+	printf("%d\n",ret_code);
 
-		puts("Array read successfully, contents: ");
-		for(int n = 0; n < size; ++n) printf("%X ", in[n]);
-		putchar('\n');
+	printf("Array read successfully, contents: \n");
+	for(int n = 0; n < size; ++n) printf("%X ", in[n]);
+	printf("\n");
 
 	   if (feof(fp))
 			printf("Error reading %s: unexpected end of file\n",name);
 	   else if (ferror(fp)) 
-			printf("Error reading %s", name);
-	
+			printf("Error reading %s", name);	
+}
+
+void main(void){
+	int choice;
+	do{
+		printf("Action:\n1. Encrypt Message\n2. Encrypt Binary File\n3. Exit\nYour choice: ");
+		scanf("%d", &choice);
+		//myFlush();
+		printf("\n--------------------------------------------------------------\n");
+	}while(choice < 1 || choice > 3);
+	(choice == 1)? 0: (choice == 2) ? encryptFile("test.bin") :  (choice == 3) ? exit(0) : 0;
 }
