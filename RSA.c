5caff99c0764410ec624a8d6bc5b4ddc9a69cb6b
Surfaze <life.simplified@hotmail.com>
2016-01-04 06:27:58 +0800

    Decryption solved
    using old Struct array

--- RSA.c
+++ RSA.c
@@ -20,6 +20,12 @@ typedef struct{
 	int d;
 } Keys;
 
+typedef struct{
+	long *array;
+	size_t size;
+} Array;
+
+
 //Function headers
 void menu();
 void myFlush();
@@ -43,6 +49,8 @@ Keys readPrivKey2();
 char* getEncryptedFileName();
 char* readEncrypted();
 void decryptBytes(int* arr, int size, Keys key);
+void initArray(Array *a, size_t initialSize);
+void freeArray(Array *a);
 
 
 //Main
@@ -570,17 +578,19 @@ void decrypt(){
 		int numberOfChara = 0;
 		
 		while(token != NULL){
-			printf("%s\n", token);
+			//printf("%s\n", token);
 			token = strtok(NULL, tok);
 			numberOfChara++;
 		}
 		
-		//printf(" number of chara = %d",numberOfChara);
+		printf(" number of chara = %d",numberOfChara);
 		//numberOfChara--; //because it will ++ 1 more time after while loop
 		
 		
 		//init the array
-		int* ctArray = (int*)malloc(sizeof(numberOfChara));
+		//long* ctArray = (long*)malloc(sizeof(numberOfChara));
+		Array ctArray;
+		initArray(&ctArray, numberOfChara);
 		
 		//populate
 		rewind(fp);
@@ -591,24 +601,55 @@ void decrypt(){
 		
 		int count = 0;
 		while(token != NULL){
-			//printf("token = %s and count = %d\n", token, count);
-			ctArray[count] = strtol(token, NULL, 10); //convert into long and store
+			printf("token = %s and count = %d\n", token, count);
+			
+			//ctArray[count] = strtol(token, NULL, 10); //convert into long and store
+			ctArray.array[count] = strtol(token, NULL, 10); //convert into long and store
+			
+			
+			//printf("ctArray%d = %ld\n",count,ctArray[count]);
 			token = strtok(NULL, tok);
 			count++;
 		}
+		count--;
 		
 		//decrypt
 		char PT[1000],print[1000];
 		
-		printf("\n--------------------------------------------------------------\n");	
-		printf("\n2 Decrypted message is:\n\"");
-		for(int i = 1; i<numberOfChara; i++){
-			PT[i] = bin_mod(ctArray[i-1], key.d, key.n);
+		
+		printf("\n--------------------------------------------------------------\n");
+		printf("Decrypted message is \"");
+		for(int i = 1; i<=numberOfChara; i++){
+			PT[i] = bin_mod(ctArray.array[i-1], key.d, key.n);
 			print[i-1] = PT[i] + '\0';
 			printf("%c", print[i-1]);
 		}
+		printf("\".\n");
+		freeArray(&ctArray);
+		
+		
+		
+		
+		
+		/* 
+		
+		
+		//printf("count = %d\nctArray = %s",count,ctArray);
+		printf("\n--------------------------------------------------------------\n");	
+		printf("\n2 Decrypted message is:\n\"");
+		for(int i = 1; i<=numberOfChara; i++){
+			//PT[i] = bin_mod(ctArray[i], key.d, key.n);
+			PT[i] = bin_mod(ctArray.array[i-1], key.d, key.n);
+			
+			print[i-1] = PT[i] + '\0';
+			printf("%c", print[i]);
+		}
 		printf("\"\n");
-		free(ctArray);
+		//free(ctArray);
+		freeArray(&ctArray); 
+		
+		
+		*/
 		
 		menu();
 		
@@ -650,6 +691,18 @@ void decryptBytes(int* arr, int size, Keys key){
 		printf("%d\n",arr[i]);
 		arr[i] = bin_mod(arr[i],key.d,key.n);
 	}
+	
+}
+
+void initArray(Array *a, size_t initialSize){
+	a->array = (long*)malloc(initialSize * sizeof(long));
+	a->size = initialSize;
+	
+}
+void freeArray(Array *a){
+	free(a->array);
+	a->array = NULL;
+	a->size = 0;
 	
 }
 //=============================End Jia Hao's Functions======================================
